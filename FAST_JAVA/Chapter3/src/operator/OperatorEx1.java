6185ad20a351d371b9e005d4e49d3cf241a6cc60
jih3508 <whdlrgus32@naver.com>
2021-04-09 20:40:43 +0900

    Chapter3

---
+++ FAST_JAVA/Chapter3/src/operator/OperatorEx1.java
@@ -0,0 +1,23 @@
+package operator;
+
+public class OperatorEx1 {
+
+	public static void main(String[] args) {
+		
+		int num1 = -10;
+		int num2 = 20;
+		
+		System.out.println(+num1);
+		System.out.println(+num2);
+		
+		System.out.println(-num1);
+		System.out.println(-num2);
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		int a = 2048;
+		System.out.println(a%2047);
+	}
+
+}
---
+++ FAST_JAVA/Chapter3/src/operator/OperatorEx4.java
@@ -0,0 +1,22 @@
+package operator;
+
+public class OperatorEx4 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 0B00001010;
+		int num2 = 0B00000101;
+		
+		System.out.println(num1 & num2);
+		System.out.println(num1 | num2);
+		System.out.println(num1 ^ num2);
+		
+		System.out.println(num2 << 1);
+		System.out.println(num2 << 2);
+		System.out.println(num2 << 3);
+		System.out.println(num2);
+		
+		System.out.println(num2 >> 21;
+	}
+
+}
