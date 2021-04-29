fc1c70444f4e64e22954dc2655f7faf73d6d0215
jih3508 <whdlrgus32@naver.com>
2021-04-29 22:29:12 +0900

    chapter11

---
+++ FAST_JAVA/Chapter11/src/collection/treeset/MemberTreeSet.java
@@ -0,0 +1,38 @@
+package collection.treeset;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class MemberTreeSet {
+
+	private TreeSet<Member> treeSet;
+	
+	public MemberTreeSet() {
+		//Comparator 작성할때 괄호안에 new Member() 작성 새로 생성된 객체와 이전 객체랑 비교함
+		treeSet = new TreeSet<Member>(new Member()); 
+	}
+	
+	public void addMember(Member member) {
+		treeSet.add(member);
+	}
+	
+	public boolean removeMember(int memberId) {
+		Iterator<Member> ir = treeSet.iterator();
+		while(ir.hasNext()) {
+			Member member = ir.next();
+			if(member.getMemberId() == memberId) {
+				treeSet.remove(member);
+				return true;
+			}
+		}
+		System.out.println(memberId + "번호가 존재하지 않습니다.");
+		return false;
+	}
+	
+	public void showAllMember() {
+		for(Member member : treeSet) {
+			System.out.println(member);
+		}
+		System.out.println();
+	}
+}
