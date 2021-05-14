26d5446c0d4e29a7a0fb56ad09c72a9f5120fa3f
jih3508 <whdlrgus32@naver.com>
2021-05-15 02:29:09 +0900

    JPA를 통한 CRUD 배우기

---
+++ Admin/src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,77 @@
+package com.example.study.repository;
+
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+
+public class UserRepositoryTest  extends StudyApplicationTests {
+
+    // Dependency Injection(DI)
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public  void create(){
+        // String sql = insert into user(%s, %s, %d) value (account, email, age);
+        User user = new User();
+
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
+        user.setPhoneNumber("010-1111-1111");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("TestUser03");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser : " + newUser);
+    }
+
+    @Test
+    public void read(){
+
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{ //ifPresent : null이 아닌경우 로직을 돌린다.
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
+
+    }
+
+    @Test
+    public void update(){
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{ // 기존 저장한 값에서 수정된 값을 저장 한다.
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
+    }
+
+    @Test
+    @Transactional // 실행하고 원래 상태로 되돌려 준다.
+    public void delete(){
+        Optional<User> user = userRepository.findById(3L);
+
+        Assertions.assertTrue(user.isPresent()); //true
+
+        user.ifPresent(selectUser ->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        Assertions.assertFalse(deleteUser.isPresent()); // False
+    }
+}
