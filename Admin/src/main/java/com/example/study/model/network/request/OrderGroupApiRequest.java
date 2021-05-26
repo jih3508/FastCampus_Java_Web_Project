8d559aee45971bb2c75bfbd976e9af99e8cb805c
jih3508 <whdlrgus32@naver.com>
2021-05-27 00:01:02 +0900

    주문내역 API UPDATE

---
+++ Admin/src/main/java/com/example/study/model/network/request/OrderGroupApiRequest.java
@@ -0,0 +1,39 @@
+package com.example.study.model.network.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderGroupApiRequest {
+
+    private Long id;
+
+    private String status;
+
+    private String orderType;
+
+    private String revAddress;
+
+    private String revName;
+
+    private String paymentType;
+
+    private BigDecimal totalPrice;
+
+    private Integer totalQuantity;
+
+    private LocalDateTime orderAt;
+
+    private LocalDateTime arrivalDate;
+
+    private Long userId;
+}
