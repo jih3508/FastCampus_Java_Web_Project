e8dcdc25c86d118f5b90bdfb6c70106e102552b6
jih3508 <whdlrgus32@naver.com>
2021-06-12 18:05:40 +0900

    Springboot 지인관리 프로그램 완강

--- mycontact/src/main/java/com/fastcampus/javaallinon/mycontact/controller/PersonController.java
+++ mycontact/src/main/java/com/fastcampus/javaallinon/mycontact/controller/PersonController.java
@@ -9,11 +9,16 @@ import com.fastcampus.javaallinon.mycontact.repository.PersonRepository;
 import com.fastcampus.javaallinon.mycontact.service.PersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.xml.ws.Response;
+import java.util.List;
 
 @RequestMapping(value = "/api/person")
 @RestController
@@ -23,6 +28,11 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+    @GetMapping
+    public Page<Person> getAll(@PageableDefault Pageable pageable){
+        return personService.getAll(pageable);
+    }
+
     @GetMapping("{id}")
     public Person getPerson(@PathVariable Long id){
         return personService.getPerson(id);
@@ -30,7 +40,7 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void postPerson(@RequestBody PersonDto personDto){
+    public void postPerson(@RequestBody @Valid PersonDto personDto){
         personService.put(personDto);
 
     }
